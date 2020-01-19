8bdb9c27910cde157f66734f1418477ee481667a
Matheus Pratta <matpratta@gmail.com>
2020-01-19 16:28:02 -0300

    Removed dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This is the entry point file. It's only used for quick access to other features.
- */
-import Router from './Router';
-import Request from './Request';
-export { Router, Request, };
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * This is the entry point file. It's only used for quick access to other features.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-var Router_1 = require("./Router");
-exports.Router = Router_1.default;
-var Request_1 = require("./Request");
-exports.Request = Request_1.default;
